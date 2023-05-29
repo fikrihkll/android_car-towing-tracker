bd35f542823f51e8445f401a0c1330edc062de16
zackmolt <fkrihkl@gmail.com>
2023-05-29 10:57:47 +0700

    feat: create entity

---
+++ app/src/main/java/com/dagger/parceltrackingui/entity/ServiceEntity.kt
@@ -0,0 +1,9 @@
+package com.dagger.parceltrackingui.entity
+
+import androidx.compose.ui.graphics.Color
+
+data class ServiceEntity (
+    val icon: String,
+    val name: String,
+    var color: Color
+)
