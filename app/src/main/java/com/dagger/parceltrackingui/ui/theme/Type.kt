7c15524ec54714602130af8ee58c1032f72e2233
zackmolt <fkrihkl@gmail.com>
2023-05-29 10:58:31 +0700

    feat: add new typography

--- app/src/main/java/com/dagger/parceltrackingui/ui/theme/Type.kt
+++ app/src/main/java/com/dagger/parceltrackingui/ui/theme/Type.kt
@@ -26,6 +26,12 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.0.sp
     ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.0.sp
+    ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
@@ -41,6 +47,12 @@ val Typography = Typography(
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 10.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
