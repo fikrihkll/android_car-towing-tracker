7a291ebe856dd2bc05ac9dc74085722c90cb9d33
zackmolt <fkrihkl@gmail.com>
2023-06-26 17:25:40 +0700

    refactor: finalize ui

--- app/src/main/java/com/dagger/parceltrackingui/ui/home/HomeScreen.kt
+++ app/src/main/java/com/dagger/parceltrackingui/ui/home/HomeScreen.kt
@@ -59,6 +59,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -87,18 +88,23 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.dagger.parceltrackingui.R
 import com.dagger.parceltrackingui.entity.ServiceEntity
 import com.dagger.parceltrackingui.ui.components.Shimmer
+import com.dagger.parceltrackingui.ui.home.components.BigAppBar
+import com.dagger.parceltrackingui.ui.home.components.DeliveryCardSection
 import com.dagger.parceltrackingui.ui.home.components.LargeIconButton
 import com.dagger.parceltrackingui.ui.home.components.TOP_LEVEL_DESTINATIONS
+import com.dagger.parceltrackingui.ui.home.viewmodels.HomeDeliveryListState
+import com.dagger.parceltrackingui.ui.home.viewmodels.HomeDeliveryListViewModel
 import com.dagger.parceltrackingui.ui.theme.ParcelTrackingUITheme
 import com.dagger.parceltrackingui.ui.theme.cAccent
-import com.dagger.parceltrackingui.ui.theme.cGreen
 import com.dagger.parceltrackingui.ui.theme.cBrownLightPastel
 import com.dagger.parceltrackingui.ui.theme.cMintGreenPastel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -109,16 +115,20 @@ fun HomeScreen(
         parcelId: String,
         parcelType: String,
         enableChat: Boolean
-    ) -> Unit
+    ) -> Unit,
+    viewModel: HomeDeliveryListViewModel = viewModel()
 ) {
 
-    var selectedTab by rememberSaveable {
-        mutableStateOf("Current")
-    }
+    val homeDeliveryListState by viewModel.homeDeliveryListDataState.collectAsState()
     var selectedDestination by rememberSaveable {
         mutableStateOf(
             TOP_LEVEL_DESTINATIONS.first().route
         )
+    }
+    LaunchedEffect(key1 = Unit) {
+        if (homeDeliveryListState is HomeDeliveryListState.HomeDeliveryInitial) {
+            viewModel.loadHomeDeliveryList("Current")
+        }
     }
 
     Scaffold(
@@ -136,7 +146,13 @@ fun HomeScreen(
                     )
                 }
                 item {
-                    DeliveryCardSection()
+                    DeliveryCardSection(
+                        isLoading = homeDeliveryListState is HomeDeliveryListState.HomeDeliveryLoading,
+                        listData = (homeDeliveryListState as? HomeDeliveryListState.HomeDeliveryLoaded)?.listData
+                            ?: emptyList()
+                    ) {
+                        viewModel.loadHomeDeliveryList(it)
+                    }
                 }
                 item {
                     Column(
@@ -197,491 +213,6 @@ fun HomeScreen(
             }
         },
     )
-}
-
-@Composable
-fun DeliveryCardSection() {
-    var selectedTab by rememberSaveable {
-        mutableStateOf("Current")
-    }
-    Column {
-        HomeParcelListTabBar(
-            selectedTab,
-            listOf("Current", "History", "All"),
-            onTabClick = {
-                selectedTab = it
-            }
-        )
-        DeliveringPackageTrackingCard()
-    }
-}
-
-@Composable
-fun HomeParcelListTabBar(
-    selectedTab: String,
-    tabs: List<String>,
-    onTabClick: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    LazyRow(
-        modifier = modifier
-            .padding(vertical = 8.dp),
-        contentPadding = PaddingValues(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        items(tabs) {
-            ParcelListTab(selectedTab = selectedTab, tab = it, onTabClick = onTabClick)
-        }
-    }
-}
-
-@Composable
-fun ParcelListTab(
-    onTabClick: (String) -> Unit,
-    selectedTab: String,
-    tab: String,
-) {
-    val scaleTransition = updateTransition(
-        selectedTab,
-        label = "Tab Indicator"
-    )
-    val scale by scaleTransition.animateFloat(
-        label = "scale animation",
-        targetValueByState = { value ->
-            if (value == tab) {
-                1.2f
-            } else {
-                1f
-            }
-        }
-    )
-    val padding by scaleTransition.animateDp(
-        label = "extra padding",
-        targetValueByState = { value ->
-            if (value == tab) {
-                12.dp
-            } else {
-                0.dp
-            }
-        }
-    )
-
-    val isSelected = selectedTab == tab
-
-    Box(
-        modifier = Modifier
-            .clip(RoundedCornerShape(16.dp))
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(color = MaterialTheme.colorScheme.primary),
-                enabled = true,
-                role = Role.Tab,
-                onClick = {
-                    onTabClick(tab)
-                }
-            )
-            .padding(horizontal = padding)
-            .scale(scale)
-    ) {
-        Text(
-            tab,
-            color = if (isSelected) Color.Black else Color.Gray,
-            fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
-            modifier = Modifier
-                .padding(2.dp)
-        )
-    }
-}
-
-@Composable
-fun BigAppBar(
-    modifier: Modifier = Modifier,
-    onSearchClicked: (String) -> Unit,
-    onNavigateToDetail: (
-        parcelId: String,
-        parcelType: String,
-        enableChat: Boolean
-    ) -> Unit
-) {
-    val translationYAnimation = remember {
-        Animatable(-250f)
-    }
-    var translationYState by rememberSaveable {
-        mutableStateOf(-250f)
-    }
-    val translationYTarget = 0f
-
-    val alphaAnimation = remember {
-        Animatable(0f)
-    }
-    var alphaState by rememberSaveable {
-        mutableStateOf(0f)
-    }
-    val alphaTarget = 1f
-
-    LaunchedEffect(Unit) {
-        launch {
-            translationYAnimation.animateTo(
-                targetValue = translationYTarget,
-                animationSpec = tween(
-                    durationMillis = 1300,
-                    easing = FastOutSlowInEasing
-                )
-            )
-            translationYState = translationYTarget
-        }
-        launch {
-            alphaAnimation.animateTo(
-                targetValue = alphaTarget,
-                animationSpec = tween(
-                    durationMillis = 900,
-                    easing = FastOutSlowInEasing
-                )
-            )
-            alphaState = alphaTarget
-        }
-    }
-
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 0.dp,
-            topEnd = 0.dp,
-            bottomEnd = 32.dp,
-            bottomStart = 32.dp
-        ),
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.primary,
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 16.dp)
-                .run {
-                    if (translationYState == translationYTarget)
-                        this.offset(x = 0.dp, y = translationYState.dp)
-                    else
-                        this.offset(x = 0.dp, y = translationYAnimation.value.dp)
-                }
-                .run {
-                    if (alphaState == alphaTarget)
-                        this.alpha(alphaTarget)
-                    else
-                        this.alpha(alphaAnimation.value)
-
-                }
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Menu,
-                    contentDescription = "sidebar button"
-                )
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data("https://avatars.githubusercontent.com/u/57880863?v=4")
-                        .crossfade(true)
-                        .build(),
-                    placeholder = painterResource(
-                        id = R.drawable.ic_launcher_background
-                    ),
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(32.dp)
-                        .wrapContentSize(),
-                    contentDescription = "avatar image",
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Text("Welcome, Miguel",
-                style = MaterialTheme.typography.titleMedium,
-            )
-            Text(
-                "Find your package",
-                color = Color.LightGray
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            SearchEditText(
-                onSearchClicked = onSearchClicked,
-                onNavigateToDetail = onNavigateToDetail
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-@Composable
-fun SearchEditText(
-    onSearchClicked: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    onNavigateToDetail: (
-        parcelId: String,
-        parcelType: String,
-        enableChat: Boolean
-    ) -> Unit
-) {
-    var isFocused by rememberSaveable {
-        mutableStateOf(false)
-    }
-    val focusRequester = remember { FocusRequester() }
-    var trackingNumber by rememberSaveable {
-        mutableStateOf("")
-    }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val localFocusManager = LocalFocusManager.current
-
-    LaunchedEffect(key1 = isFocused) {
-        if (isFocused) {
-            delay(600)
-            focusRequester.requestFocus()
-        }
-    }
-
-    Surface(
-        modifier = modifier,
-        shape = RoundedCornerShape(CornerSize(32.dp))
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            TextField(
-                value = "",
-                onValueChange = {},
-                enabled = false,
-                placeholder = { Text("Tracking number", color = Color.White) },
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.Gray,
-                    disabledTextColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Gray,
-                    unfocusedIndicatorColor = Color.Gray,
-                    disabledIndicatorColor = Color.Transparent,
-                    containerColor = MaterialTheme.colorScheme.secondary
-                ),
-                shape = RoundedCornerShape(corner = CornerSize(32.dp)),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        isFocused = true
-                    }
-            )
-            AnimatedVisibility(
-                modifier = Modifier,
-                visible = isFocused,
-                enter = slideInHorizontally(
-                    initialOffsetX = { 9999 },
-                    animationSpec = tween(durationMillis = 500)
-                ),
-                exit = slideOutHorizontally(
-                    targetOffsetX = { 0 },
-                    animationSpec = tween(durationMillis = 500)
-                ),
-            ) {
-                TextField(
-                    value = trackingNumber,
-                    onValueChange = {
-                        trackingNumber = it
-                    },
-                    placeholder = { Text("Tracking number") },
-                    colors = TextFieldDefaults.textFieldColors(
-                        textColor = Color.Black,
-                        disabledTextColor = Color.Transparent,
-                        focusedIndicatorColor = Color.White,
-                        unfocusedIndicatorColor = Color.White,
-                        disabledIndicatorColor = Color.Transparent,
-                        containerColor = Color.White
-                    ),
-                    shape = RoundedCornerShape(corner = CornerSize(32.dp)),
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                    keyboardActions = KeyboardActions(
-                        onSearch = {
-                            keyboardController?.hide()
-                            localFocusManager.clearFocus()
-                            onNavigateToDetail.invoke(
-                                trackingNumber.ifEmpty { "882734" },
-                                "goods",
-                                true
-                            )
-                        },
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .focusRequester(focusRequester)
-                        .zIndex(0.9f)
-                )
-            }
-            LargeIconButton(
-                onClick = { onSearchClicked("test") },
-                color = Color.White,
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Search,
-                    contentDescription = "",
-                    tint = MaterialTheme.colorScheme.primary
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun DeliveringPackageTrackingCard(
-    modifier: Modifier = Modifier,
-) {
-    val translateXAnimation = remember {
-        Animatable(150f)
-    }
-    var translateXState by rememberSaveable {
-        mutableStateOf(150f)
-    }
-    val translateXTarget = 0f
-
-    val alphaAnimation = remember {
-        Animatable(0f)
-    }
-    var alphaState by rememberSaveable {
-        mutableStateOf(0f)
-    }
-    val alphaTarget = 1f
-
-
-    var isValueLoaded by rememberSaveable {
-        mutableStateOf(false)
-    }
-
-    LaunchedEffect(key1 = Unit) {
-        delay(4500)
-        isValueLoaded = true
-    }
-
-    LaunchedEffect(key1 = Unit) {
-        launch {
-            translateXAnimation.animateTo(
-                translateXTarget,
-                animationSpec = tween(
-                    durationMillis = 700,
-                    easing = FastOutSlowInEasing,
-                    delayMillis = 600
-                )
-            )
-            translateXState = translateXTarget
-        }
-        launch {
-            alphaAnimation.animateTo(
-                1f,
-                animationSpec = tween(
-                    durationMillis = 800,
-                    easing = FastOutSlowInEasing,
-                    delayMillis = 600
-                )
-            )
-            alphaState = alphaTarget
-        }
-    }
-
-    Surface(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .run {
-                if (alphaState == alphaTarget)
-                    this.alpha(alphaState)
-                else
-                    this.alpha(alphaAnimation.value)
-            }
-            .run {
-                if (translateXState == translateXTarget)
-                    this.offset(x = translateXState.dp, y = 0.dp)
-                else
-                    this.offset(x = translateXAnimation.value.dp, y = 0.dp)
-            },
-        color = MaterialTheme.colorScheme.primaryContainer,
-        shape = RoundedCornerShape(size = 24.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(24.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "On The Way",
-                    style = MaterialTheme.typography.titleSmall
-                )
-                Text(
-                    text = "$76",
-                    style = MaterialTheme.typography.titleSmall,
-                    color = cGreen
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier
-                    .height(110.dp)
-            ) {
-                CirclesWithLine()
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(1f),
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Column {
-                        Text(
-                            "From",
-                            color = MaterialTheme.colorScheme.primary,
-                            style = MaterialTheme.typography.labelSmall
-                        )
-                        AnimatedContent(targetState = isValueLoaded) {
-                            if (it) {
-                                Text(
-                                    "California",
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
-                            } else {
-                                Shimmer.Rectangle()
-                            }
-                        }
-                    }
-                    Column {
-                        Text(
-                            "To",
-                            color = MaterialTheme.colorScheme.secondary,
-                            style = MaterialTheme.typography.labelSmall
-                        )
-                        AnimatedContent(
-                            isValueLoaded,
-                            transitionSpec = {
-                                fadeIn(animationSpec = tween(550, delayMillis = 90)) +
-                                        scaleIn(initialScale = 0.92f, animationSpec = tween(320, delayMillis = 90)) with
-                                        fadeOut(animationSpec = tween(90))
-                            }
-                        ) {
-                            if (it) {
-                                Text(
-                                    "Boston, CA",
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
-                            } else {
-                                Shimmer.Rectangle()
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 @Composable
@@ -844,26 +375,6 @@ fun ServiceCard(
                 fontWeight = FontWeight.SemiBold
             )
         }
-    }
-}
-
-//@Preview
-@Composable
-fun SearchBarPreview() {
-    SearchEditText(onSearchClicked = {}, onNavigateToDetail = {parcelId, parcelType, enableChat ->  })
-}
-
-//@Preview(showBackground = true,)
-@Composable
-fun HomeParcelListPreview() {
-    Column {
-        HomeParcelListTabBar(
-            "All",
-            listOf("All", "Sent Packages", "Delivering"),
-            onTabClick = {
-
-            }
-        )
     }
 }
 
